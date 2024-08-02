56ef8abf4cf1334405b16243a0ee8dd246995147
张 <wz2647@columbia.edu>
2024-08-02 00:39:05 -0400

    Initial Commit

---
+++ EmotionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmotionApp
+//
+//  Created by 张 on 2024/8/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
